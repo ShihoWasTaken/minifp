424a80830b9959d7185179eb3666e7095a3f99e2
shiho <kenny.guiougou@gmail.com>
2016-04-10 00:02:45 +0200

    erreurs si base n'existe pas

--- index.php
+++ index.php
@@ -7,62 +7,6 @@ function __autoload($classname)
     require_once("phar://minifp/" . $filename);
 }
 
-
-/*
-echo 'Configuration de la base de données' . PHP_EOL;
-echo 'La configuration par défaut est: ' . PHP_EOL;
-echo 'Hôte: localhost' . PHP_EOL;
-echo 'Utilisateur: root' . PHP_EOL;
-echo 'Mot de passe: root' . PHP_EOL;
-echo 'Nom de la base: minifp' . PHP_EOL;
-
-$input = '';
-while(($input != 'oui') && ($input != 'non'))
-{	
-	echo $input;
-	echo PHP_EOL . 'Voulez-vous modifier utiliser cette configuration (oui) ou modifier les paramètres (non): ';
-	$input =  fgets(STDIN);
-	$input = trim ($input);
-}
-if($input == 'oui')
-{
-	echo 'Entrez l\'hôte MySQL ou appuyez sur entrée pour localhost: ' . PHP_EOL;
-	$host = fgets(STDIN);
-	if($host == PHP_EOL)
-		$host = 'localhost';
-	echo 'Entrez l\'user MySQL ou appuyez sur entrée pour root: ' . PHP_EOL;
-	$user = fgets(STDIN);
-	if($user == PHP_EOL)
-		$user = 'root';
-	echo 'Entrez le mot de passe MySQL ou appuyez sur entrée pour root: ' . PHP_EOL;
-	$password = fgets(STDIN);
-	if($password == PHP_EOL)
-		$password = 'root';
-	echo 'Entrez le nom de la base MySQL ou appuyez sur entrée pour minifp: ' . PHP_EOL;
-	$databaseName = fgets(STDIN);
-	if($databaseName == PHP_EOL)
-		$databaseName = 'minifp';
-}
-else
-{
-	$host = 'localhost';
-	$user = 'root';
-	$password = 'root';
-	$databaseName = 'minifp';
-}
-
-
-echo $host . PHP_EOL;
-echo $user . PHP_EOL;
-echo $password . PHP_EOL;
-echo $databaseName . PHP_EOL;
-
-while($f = fgets(STDIN)){
-    echo "line: $f";
-}
-*/
-
-
 /* Cas où minifp est appelé sans commande */
 if($argc == 1)
 {
