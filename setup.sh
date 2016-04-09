00dd77006c1d8a10149846458202d02f94b6bfb1
shiho <kenny.guiougou@gmail.com>
2016-04-09 22:34:53 +0200

    réécriture setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # On lance le script PHP pour compiler les sources en PHAR
+echo "Compilation des sources en archive PHAR..."
 \php compile.php
+echo "Compilation terminée !"
 
 # On renomme l'archive .phar sans extension
 \mv minifp.phar minifp
@@ -10,6 +12,7 @@
 \chmod +x minifp
 
 # On le déplace pour l'utiliser de manière globale peu importe le repertoire où l'on se trouve
+echo "Déplacement du programme minifp dans /user/bin"
 sudo \mv minifp /usr/bin/minifp
-
-
+echo "Minifp est maintenant installé sur votre machine"
+echo "Vous pouvez le lancer via la commande minifp"
